b7f6ce3f30c43145416f689d004fb42f2e2a11e3
dsiguero <26867647+dsiguero@users.noreply.github.com>
2025-04-13 02:20:27 +0200

    wip

--- main.tf
+++ main.tf
@@ -112,9 +112,7 @@ module "lambda" {
   publish = true
 
   environment_variables = {
-    SLACK_WEBHOOK_URL = var.slack_webhook_url
     SLACK_CHANNEL     = var.slack_channel
-    SLACK_EMOJI       = var.slack_emoji
     SLACK_BOT_TOKEN   = var.slack_bot_token
     LOG_EVENTS        = var.log_events ? "True" : "False"
   }
